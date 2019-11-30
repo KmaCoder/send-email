465cc9d0b7a2b9061b42302d2ad402f322ee29b3
Maksym <maxim4110@gmail.com>
2019-11-30 13:56:12 +0200

    fix

--- main.py
+++ main.py
@@ -3,35 +3,36 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from dotenv import load_dotenv
 import os
+import time
 
 load_dotenv()
 
 if __name__ == "__main__":
-    msg = MIMEMultipart('alternative')
-    msg['Subject'] = os.getenv("SUBJECT")
-    msg['From'] = os.getenv("SENDER")
-    msg['To'] = os.getenv("RECEIVER")
-
-    part1 = MIMEText(msg['Subject'], 'plain')
-    with open("email.html", 'r') as html_file:
-        part2 = MIMEText(html_file.read(), 'html')
-
-    msg.attach(part1)
-    msg.attach(part2)
-
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
     server.login(
         os.getenv("SENDER"),
         os.getenv("PASSWORD")
     )
 
+    msg_part1 = MIMEText(os.getenv("SUBJECT"), 'plain')
+    with open("email.html", 'r') as html_file:
+        msg_part2 = MIMEText(html_file.read(), 'html')
+
     receivers = os.getenv("RECEIVER").split(',')
     for receiver in receivers:
+        msg = MIMEMultipart('alternative')
+        msg['Subject'] = os.getenv("SUBJECT")
+        msg['To'] = receiver
+        msg['From'] = os.getenv("SENDER")
+        msg.attach(msg_part1)
+        msg.attach(msg_part2)
+
         server.sendmail(
             os.getenv("SENDER"),
             receiver,
             msg.as_string()
         )
         print(f"Email sent to {receiver} successfully")
+
     server.quit()
     print(f"\n{len(receivers)} emails sent successfully")
